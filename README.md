c7da1c1cdcef3245b327f21085e40991baad07b0
Alice Meng <mengx520@gmail.com>
2023-11-19 15:06:12 -0800

    create packaging files for game

---
+++ README.md
@@ -0,0 +1,20 @@
+# README
+## Building package
+
+```
+python3 -m pip install --upgrade build
+python3 -m build
+```
+https://packaging.python.org/en/latest/tutorials/packaging-projects/ 
+
+## Installing package
+```
+pip install -e .
+or
+pip install dist/*.whl
+```
+
+## Running game
+```
+first_game
+```
---
+++ src/first_game/game.py
@@ -0,0 +1,26 @@
+import pygame
+
+def run():
+    pygame.init()
+
+    screen = pygame.display.set_mode((1280,720))
+
+    clock = pygame.time.Clock()
+
+    while True:
+        # Process player inputs.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+
+        # Do logical updates here.
+        # ...
+
+        screen.fill("black")  # Fill the display with a solid color
+
+        # Render the graphics here.
+        # ...
+
+        pygame.display.flip()  # Refresh on-screen display
+        clock.tick(60)         # wait until next frame (at 60 FPS)
