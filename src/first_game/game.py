9597a9bd7acc5221c28f3763c9611e44e063b449
Alice Meng <mengx520@gmail.com>
2023-11-19 15:56:25 -0800

    add basic player movement

--- src/first_game/game.py
+++ src/first_game/game.py
@@ -1,26 +1,46 @@
 import pygame
 
 def run():
+    # pygame setup
     pygame.init()
-
+    
     screen = pygame.display.set_mode((1280,720))
-
     clock = pygame.time.Clock()
+    running = True
+    delta_time = 0
 
-    while True:
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    player_speed = 300
+
+    while running:
         # Process player inputs.
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                raise SystemExit
+                running = False
 
         # Do logical updates here.
-        # ...
-
-        screen.fill("black")  # Fill the display with a solid color
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player_pos.y -= player_speed * delta_time
+        if keys[pygame.K_s]:
+            player_pos.y += player_speed * delta_time
+        if keys[pygame.K_a]:
+            player_pos.x -= player_speed * delta_time
+        if keys[pygame.K_d]:
+            player_pos.x += player_speed * delta_time
+            
+        # Fill the display with a solid color
+        screen.fill("black")  
 
         # Render the graphics here.
-        # ...
+        pygame.draw.circle(screen, "white", player_pos, 15)
 
-        pygame.display.flip()  # Refresh on-screen display
-        clock.tick(60)         # wait until next frame (at 60 FPS)
+        # Refresh on-screen display
+        pygame.display.flip()  
+        # wait until next frame (at 60 FPS)
+        # dt is delta time in seconds since last frame, used for framerate-independent physics.
+        delta_time = clock.tick(60) / 1000        
+    
+    pygame.quit()
