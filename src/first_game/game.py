7f492e2e829b88cf9f83e28dcfd6c30e5d655d29
Alice Meng <mengx520@gmail.com>
2023-11-19 17:02:05 -0800

    add logic to shoot projectiles

--- src/first_game/game.py
+++ src/first_game/game.py
@@ -9,8 +9,15 @@ def run():
     running = True
     delta_time = 0
 
+    # player variables
     player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
     player_speed = 300
+
+    player_projectiles = []
+    projectiles_speed = 400
+    # delay the projectile creation speed
+    projectiles_delay = 500
+    prev_projectile_time = 0
 
     while running:
         # Process player inputs.
@@ -20,7 +27,9 @@ def run():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Do logical updates here.
+        # Do logical updates here
+        # player logic
+        # player movements
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             player_pos.y -= player_speed * delta_time
@@ -30,12 +39,40 @@ def run():
             player_pos.x -= player_speed * delta_time
         if keys[pygame.K_d]:
             player_pos.x += player_speed * delta_time
-            
+
+        # projectiles logic
+        if keys[pygame.K_SPACE]:
+            # check if enough time has passed before creating new projectile
+            if pygame.time.get_ticks() - prev_projectile_time > projectiles_delay:
+                # new prjectile starts from above player
+                new_projectile = pygame.Vector2(player_pos.x,  player_pos.y - 8)
+                player_projectiles.append(new_projectile)
+                # saving the previous projectile creation time
+                prev_projectile_time = pygame.time.get_ticks()
+
+        # projectile movement
+        # loop throught front the end of the list to avoid bad indexes
+        for i in range(len(player_projectiles) - 1, -1, -1):
+            p = player_projectiles[i]
+            p.y -= projectiles_speed * delta_time
+            # destroy projectile when it goes beyond screen
+            if p.y < -15:
+                del player_projectiles[i]
+
         # Fill the display with a solid color
         screen.fill("black")  
 
         # Render the graphics here.
+        # draw player 
         pygame.draw.circle(screen, "white", player_pos, 15)
+
+        # draw projectiles
+        for p in player_projectiles:
+            # initialized position to draw projectiles
+            projectile_rect = pygame.Rect(p.x, p.y, 3, 10)
+            # draw projectiles
+            pygame.draw.rect(screen, "red", projectile_rect)
+
 
         # Refresh on-screen display
         pygame.display.flip()  
